1c2eee3a921c1c24018cd8ec4333e93ca311ca2e
Nicholas Geismar <ncgeismar@gmail.com>
2021-03-30 17:30:27 -0400

    port 443

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require('./server/server.js');
 const {db} = require('./server/database');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 443;
 
 db.sync()
   .then(function(){
